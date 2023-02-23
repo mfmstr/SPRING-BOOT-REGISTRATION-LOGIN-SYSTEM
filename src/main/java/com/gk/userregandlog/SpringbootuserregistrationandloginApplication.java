65b3ec12d642e5e9bc4e7629c81e6d9d89199f7f
mfmstr <g.kharebava01@gmail.com>
2023-02-23 18:42:42 +0400

    Adding Exclude In SpringBootApplication Annotation

--- src/main/java/com/gk/userregandlog/SpringbootuserregistrationandloginApplication.java
+++ src/main/java/com/gk/userregandlog/SpringbootuserregistrationandloginApplication.java
@@ -2,8 +2,9 @@ package com.gk.userregandlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringbootuserregistrationandloginApplication {
 
 	public static void main(String[] args) {
