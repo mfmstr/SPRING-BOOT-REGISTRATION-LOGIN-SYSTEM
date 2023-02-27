b0ae4cf5c2a933c400fd7c786217d4f25ea7ea11
mfmstr <g.kharebava01@gmail.com>
2023-02-28 02:46:01 +0400

    Adding CustomUserDetailsService class

--- src/main/java/com/gk/userregandlog/repository/UserRepository.java
+++ src/main/java/com/gk/userregandlog/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.gk.userregandlog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.gk.userregandlog.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
 
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	User findByEmail(String email);
+	
 }
